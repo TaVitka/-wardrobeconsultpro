f67eae452c566882c5999486f401fc3ace340030
TaVitka <tavitka17@gmail.com>
2024-05-03 17:36:41 +0300

    added reviews section with slider for homepage

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,6 +1,5 @@
 import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
 
-
 let options = {
   slidesPerView: 1,
   spaceBetween: 16,
@@ -22,10 +21,10 @@ const swiperClient = new Swiper('.slider__client', options);
 // const swiperBlog = new Swiper('.slider__blog', options);
 // const swiperEvent = new Swiper('.slider__event', options);
 
-const swiperOutfit = new Swiper('.slider__outfit', {
-  slidesPerView: 2,
-  spaceBetween: 64,
-  slidesPerGroup: 2,
+
+const swiperReviews = new Swiper('.slider__reviews', {
+  slidesPerView: 1,
+  spaceBetween: 24,
   speed: 700,
   autoplay: {
     delay: 50000,
@@ -37,14 +36,39 @@ const swiperOutfit = new Swiper('.slider__outfit', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    319: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    renderFraction: function (currentClass, totalClass, index, total) {
+
+      return `<span>0</span><span class="${currentClass}"></span> | <span>0</span><span class="${totalClass}"></span>`
+    }
   },
-});
+})
+
+// const swiperOutfit = new Swiper('.slider__outfit', {
+//   slidesPerView: 2,
+//   spaceBetween: 64,
+//   slidesPerGroup: 2,
+//   speed: 700,
+//   autoplay: {
+//     delay: 50000,
+//   },
+//   modules: [Navigation, Scrollbar, Pagination, Autoplay],
+//   draggable: true,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+//     769: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//     },
+//     319: {
+//       slidesPerView: 1,
+//       slidesPerGroup: 1,
+//     },
+//   },
+// });
